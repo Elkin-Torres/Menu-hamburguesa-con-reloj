6c02c7cc099910dcad3743e5955860da06a45927
Elkin <elkin_torres_programador@hotmail.com>
2022-08-26 21:48:57 -0500

    Versión 6

--- cambio_color.js
+++ cambio_color.js
@@ -5,9 +5,16 @@ export function cambioColor(btnCambio) {
     moon = "🌙";
   const $btnCambio = d.querySelector(btnCambio);
 
+  const colorFondo = localStorage.getItem("otroColor");
+  
+  if (colorFondo === "true") {
+    d.body.classList.toggle("dark");
+  } 
+
   d.addEventListener("click", (e) => {
     if (e.target.matches(btnCambio)) {
-      d.body.classList.toggle("dark");
+      const color = d.body.classList.toggle("dark");
+      localStorage.setItem("otroColor", color);
       if ($btnCambio.textContent === sun) {
         $btnCambio.textContent = moon;
       } else {
